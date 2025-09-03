2ce945b4b0c34cbeeaabb2d94f51c7c5f484086f
Federico Peralta <federicoperalta@Federicos-MacBook-Pro.local>
2025-09-02 21:50:11 -0300

    Clase-04: add controllers for posts

--- src/routes/post/index.ts
+++ src/routes/post/index.ts
@@ -5,5 +5,10 @@ import controllers from './controllers';
 const router = express.Router();
 
 router.post('/', controllers.createPost);
+router.get('/', controllers.getAllPosts);
+router.get('/:id', controllers.getPostById);
+router.patch('/:id', controllers.updatePost);
+router.delete('/hard/:id', controllers.hardDeletePost);
+router.patch('/soft/:id', controllers.softDeletePost);
 
 export default router;
